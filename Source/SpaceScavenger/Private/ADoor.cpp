ee5055f0e12b195472ba0a241f079e72ca26b4eb
Remus Jones <remus.jones@hotmail.com>
2023-12-11 19:59:18 +1100

    Prepared Door BP with ADoor cpp class ready for airlock system

---
+++ Source/SpaceScavenger/Private/ADoor.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ADoor.h"
+
+// Sets default values
+AADoor::AADoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AADoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AADoor::Toggle()
+{
+	bState = !bState;
+	if (DoorStateChangedDelegate.IsBound())
+	{
+		DoorStateChangedDelegate.Broadcast(bState);
+	}
+}
+
+void AADoor::Open()
+{
+	if (!bState)
+	{
+		bState = !bState;
+		if (DoorStateChangedDelegate.IsBound())
+			DoorStateChangedDelegate.Broadcast(bState);
+	}
+}
+
+void AADoor::Close()
+{
+	if (bState)
+	{
+		bState = !bState;
+		if (DoorStateChangedDelegate.IsBound())
+			DoorStateChangedDelegate.Broadcast(bState);
+	}
+}
+
+void AADoor::SetState(const bool NewState)
+{
+	if (bState != NewState)
+	{
+		if (NewState)
+			Open();
+		else
+			Close();
+	}
+}
---
+++ Source/SpaceScavenger/Public/ADoor.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ADoor.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDoorStateChangedDelegate, bool, State);
+UCLASS()
+class SPACESCAVENGER_API AADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AADoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	UFUNCTION(BlueprintCallable)
+	void Toggle();
+	UFUNCTION(BlueprintCallable)
+	void Open();
+	UFUNCTION(BlueprintCallable)
+	void Close();
+	UFUNCTION(BlueprintCallable)
+	void SetState(bool NewState);
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere	)
+	bool bState = true;
+
+	UPROPERTY(BlueprintAssignable)
+	FDoorStateChangedDelegate DoorStateChangedDelegate;
+};
