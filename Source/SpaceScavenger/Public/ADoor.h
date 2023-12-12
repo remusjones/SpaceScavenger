d80fcc79a7723c5a058b52f4aabf3b050de70834
remus.jones <remus.jones@hotmail.com>
2023-12-12 13:49:51 +1100

    Script changes missed in the last commit

--- Source/SpaceScavenger/Public/ADoor.h
+++ Source/SpaceScavenger/Public/ADoor.h
@@ -25,7 +25,7 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, CallInEditor)
 	void Toggle();
 	UFUNCTION(BlueprintCallable)
 	void Open();
